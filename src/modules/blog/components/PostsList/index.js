176e764df8e13bffb6a64debc569bf15e9d6f8ae
michaljarnot <michal.jarnot@strv.com>
2017-11-12 23:45:57 +0100

    Update post list
    - polish source code
    - use recompose in order to make the component more functional

--- src/modules/blog/components/PostsList/index.js
+++ src/modules/blog/components/PostsList/index.js
@@ -1,45 +1,50 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 import { Link } from 'react-router-dom'
+import { compose, withHandlers } from 'recompose'
 import { withApollo } from 'react-apollo'
 import { queryPostDetail } from 'modules/blog/qql'
 import { Button } from 'components'
 
 
-export const PostListComponent = ({ items, client }) => {
-  const prefetchDetail = postId => () => client.query({ // eslint-disable-line
-    query: queryPostDetail,
-    variables: { id: postId },
-  })
-
-  return (
-    <div>
-      {items.map(item => (
-        <div key={item.id}>
-          <h3>{item.title}</h3>
-          <p>{item.text}</p>
-          <Link to={`/posts/${item.id}`}>
-            <Button
-              onFocus={prefetchDetail(item.id)}
-              onMouseOver={prefetchDetail(item.id)}
-            >
-              Detail
-            </Button>
-          </Link>
-        </div>
-      ))}
-    </div>
-  )
-}
+export const PostListComponent = ({ items, handlePrefetchDetail }) => (
+  <div>
+    {items.map(item => (
+      <div key={item.id}>
+        <h3>{item.title}</h3>
+        <p>{item.text}</p>
+        <Link to={`/posts/${item.id}`}>
+          <Button
+            onFocus={handlePrefetchDetail(item.id)}
+            onMouseOver={handlePrefetchDetail(item.id)}
+          >
+            Detail
+          </Button>
+        </Link>
+      </div>
+    ))}
+  </div>
+)
 
 PostListComponent.propTypes = {
   items: PropTypes.arrayOf(PropTypes.shape({
     title: PropTypes.string.isRequired,
     text: PropTypes.string.isRequired,
   })).isRequired,
-  client: PropTypes.shape({
+  client: PropTypes.shape({ // eslint-disable-line
     query: PropTypes.func.isRequired,
   }).isRequired,
+  handlePrefetchDetail: PropTypes.func.isRequired,
 }
 
-export default withApollo(PostListComponent)
+const enhance = compose(
+  withApollo,
+  withHandlers({
+    handlePrefetchDetail: ({ client }) => (postId) => () => client.query({ // eslint-disable-line
+      query: queryPostDetail,
+      variables: { id: postId },
+    }),
+  }),
+)
+
+export default enhance(PostListComponent)
