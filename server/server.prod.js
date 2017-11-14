cab4354669fc14253b186ba4347fbbc4d4d7819c
michaljarnot <michal.jarnot@strv.com>
2017-11-14 15:01:13 +0100

    Set production server default port to 3000

--- server/server.prod.js
+++ server/server.prod.js
@@ -4,7 +4,7 @@ const compression = require('compression')
 const invariant = require('invariant')
 
 
-const PORT = process.env.PORT || 8080
+const PORT = process.env.PORT || 3000
 const DIST_DIR = path.resolve(__dirname, '..', 'public')
 
 const app = express()
